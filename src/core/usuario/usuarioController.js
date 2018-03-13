e6151fb4b761e2a141a6c24b34e1e80991202b79
Gabriel Ferreira <gabriel.ferreira@smn.com.br>
2018-03-13 17:46:47 -0300

    Helpers

--- src/core/usuario/usuarioController.js
+++ src/core/usuario/usuarioController.js
@@ -1,7 +1,9 @@
 const repository = require('./usuarioRepository');
 const Validator = require('./../../api/validator/validator');
 
-const fs = require('fs')
+const helperImages = require('../../helpers/images');
+
+
 
 module.exports = {
     insert
@@ -14,26 +16,22 @@ async function insert(req, res) {
         pass: req.body.pass,
         image: req.body.image
     };
-
     let validator = new Validator();
     validator.isRequired(params.name, 'Nome é requirido');
     validator.isRequired(params.email, 'Email é requirido');
     validator.isRequired(params.pass, 'Senha é requirida');
     validator.isEmail(params.email, 'Email invalido');
-
-
     if (!validator.isValid()) {
         return res.finish({
             httpCode: 400,
             error: validator.errors()
         });
     }
-
     try {
         await repository.verifyEmail(params.email);
-        params.image = await insertImg(params.image);
+        // params.image = await insertImg(params.image);
+        params.image = await helperImages.insertImg(params.image, 'user_', 'images/');
         await repository.insert(params);
-
         return res.finish({
             message: 'Usuario cadastrado com sucesso'
         });
@@ -45,51 +43,5 @@ async function insert(req, res) {
     }
 }
 
-async function insertImg(base64) {
-    return new Promise((resolve, reject) => {
-        let matches = base64.match(/^data:([A-Za-z-+/]+);base64,(.+)$/);
-        let response = {};
-        if (!matches) {
-            throw {
-                httpCode: 409,
-                message: 'Formato invalido',
-                executionCode: 2
-            };
-        }
-        const extencao = identifyExtencao(matches[1]);
 
-        response.data = new Buffer(matches[2], 'base64');
-        const name = `${generateNameImg()}.${extencao}`;
-
-        fs.writeFile(`images/${name}`, response.data, async(error) => {
-            if (error) {
-                reject(error);
-            }
-            resolve(name);
-        });
-    })
-}
-
-function generateNameImg() {
-    const milli = new Date().getTime();
-    return `user${milli}`;
-}
-
-function identifyExtencao(fullExt) {
-    let extencao;
-    switch (fullExt) {
-        case 'image/svg+xml':
-            extencao = 'svg';
-            break;
-        case 'image/png':
-            extencao = 'png';
-            break;
-        case 'image/jpeg':
-            extencao = 'jpg';
-            break;
-        default:
-            extencao = null;
-    }
-    return extencao;
-}
 
---
+++ src/helpers/images.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+module.exports = {
+    insertImg
+};
+
+async function insertImg(base64, prefix, path) {
+    return new Promise((resolve, reject) => {
+        let matches = base64.match(/^data:([A-Za-z-+/]+);base64,(.+)$/);
+        let response = {};
+        if (!matches) {
+            throw {
+                httpCode: 409,
+                message: 'Formato invalido',
+                executionCode: 2
+            };
+        }
+        let extencao;
+        switch (matches[1]) {
+            case 'image/svg+xml':
+                extencao = 'svg';
+                break;
+            case 'image/png':
+                extencao = 'png';
+                break;
+            case 'image/jpeg':
+                extencao = 'jpg';
+                break;
+            default:
+                extencao = null;
+        }
+        response.data = new Buffer(matches[2], 'base64');
+        const milli = new Date().getTime();
+        const random = ("0000" + (Math.floor(Math.random() * 1000) + 1)).slice(-4); // Coloca zero a esquerda
+        const name = `${prefix}${milli}_${random}.${extencao}`;
+        fs.writeFile(`${path}${name}`, response.data, async (error) => {
+            if (error) {
+                reject(error);
+            }
+            resolve(name);
+        });
+    })
+}
